fix gradient issue, add new NNmodel

Pipeline.py
@@ -176,14 +176,14 @@ class Pipeline_LC:
                 # loss3 = torch.mean(torch.abs(torch.atan2(torch.sin(angle_delta), torch.cos(angle_delta))))
                 # '''add the weight, after sovling the grad of att, we need to add the att loss the loss_all'''
                 # loss3 =  self.loss_fn(output_all[Fs:t*Fs+1, 6:], y[i, Fs:t*Fs+1, 21:])
-                # losses_train.append(loss1 + loss2 + loss3)
+                losses_train.append(loss1 +loss2 + loss3)
                 # losses_train.append(self.lcoe_p*loss1 + self.lcoe_v*loss2)
-                losses_train.append(loss3) 
+                # losses_train.append(loss3) 
 
             self.optimizer.zero_grad()
             loss_mean_train = torch.stack(losses_train).mean()
             loss_mean_train.backward()
-            # torch.nn.utils.clip_grad_norm_(self.net.parameters(),1)
+            torch.nn.utils.clip_grad_norm_(self.net.parameters(),1)
             self.optimizer.step()
             if self.scheduler is not None:
                 self.scheduler.step()

RnnModel.py
@@ -50,7 +50,7 @@ class LC_est_KG(nn.Module):
         
         self.cnn = nn.Sequential(
             nn.Conv1d(12, cnn_hidden_channels, kernel_size=5, stride=1, padding=2),
-            nn.ReLU(),
+            nn.LeakyReLU(0.01),
             nn.Conv1d(cnn_hidden_channels, cnn_out_channels, kernel_size=5, stride=1, padding=2),
             # nn.ReLU(),
             nn.Flatten()
@@ -221,7 +221,8 @@ class LC_est_KG(nn.Module):
         # f_4 = self.x_old_old
 
         f_4 = dr_temp[0:6] - meas
-        # f_5 = prev_out
+
+        f_5 = prev_out[0:6] - self.prev_out_old[0:6]
         
         # f_5 = self.x_pred_old
         f_1 = func.normalize(f_1, p=2, dim=0, eps=1e-12, out=None)
@@ -240,6 +241,7 @@ class LC_est_KG(nn.Module):
         # features_KG = torch.cat([f_1.detach(),f_2.detach(),f_3.detach(),f_5.detach()], dim=0)
 
         features_KG = torch.cat([f_1,f_2,f_3,f_4], dim=0)
+        # features_KG = torch.cat([f_1.detach(),f_2.detach(),f_3.detach(),f_4.detach()], dim=0)
 
         # KGainNet_in = KGainNet_in.detach()
         # print(KGainNet_in)
@@ -273,23 +275,37 @@ class LC_est_KG(nn.Module):
         if self.idx_feedback_type == 'pvab':
 
 
-            if sum(abs(Inno[6:])) > 0.001:
-                Inno_att_scale = Inno[6:9]*0.001
-            else:
-                Inno_att_scale = Inno[6:9]
-            # Inno_att_scale = Inno[6:9]*1e-4
-            skew_att = torch.tensor(
-                [
-                    [0, -Inno_att_scale[2], Inno_att_scale[1]],
-                    [Inno_att_scale[2], 0, -Inno_att_scale[0]],
-                    [-Inno_att_scale[1], Inno_att_scale[0], 0],
-                ]
-            )
+            # if sum(abs(Inno[6:])) > 0.001:
+            #     Inno_att_scale = Inno[6:9]*1e-3
+            # else:
+            #     Inno_att_scale = Inno[6:9]
+            Inno_att_scale = Inno[6:9]*1e-3
+            # skew_att = torch.tensor(
+            #     [
+            #         [0, -Inno_att_scale[2], Inno_att_scale[1]],
+            #         [Inno_att_scale[2], 0, -Inno_att_scale[0]],
+            #         [-Inno_att_scale[1], Inno_att_scale[0], 0],
+            #     ]
+            # )
+            skew_att = torch.zeros(3,3)
+            
+            skew_att[0,1] = -Inno_att_scale[2]
+            skew_att[0,2] = Inno_att_scale[1]
+            skew_att[1,0] = Inno_att_scale[2]
+            skew_att[1,2] = -Inno_att_scale[0]
+            skew_att[2,0] = -Inno_att_scale[1]
+            skew_att[2,1] = Inno_att_scale[0]
+
+            # skew_att_list = []
+            # skew_att_list.append
+
+
             est_C_b_e_new = (torch.eye(3) - skew_att) @ est_C_b_e_old
 
 
             dr_new_att = torch.squeeze(functions.CTM_to_euler(est_C_b_e_new.T))
             self.dr_new = torch.cat([dr_new_pv, dr_new_att, est_C_b_e_new.reshape(9)])
+
 
 
         

functions.py
@@ -297,16 +297,15 @@ def dead_reckoning_ecef(
         mag_alpha = torch.norm(alpha_ib_b)
         # print(mag_alpha)
 
-        # quit()
-        ''' in-place opreation'''
-        Alpha_ib_b = torch.tensor(
-            [
-                [0, -alpha_ib_b[2], alpha_ib_b[1]],
-                [alpha_ib_b[2], 0, -alpha_ib_b[0]],
-                [-alpha_ib_b[1], alpha_ib_b[0], 0],
-            ],
-            device=dev,
-        )
+        Alpha_ib_b = torch.zeros(3,3)
+        
+        Alpha_ib_b[0,1] = -alpha_ib_b[2]
+        Alpha_ib_b[0,2] = alpha_ib_b[1]
+        Alpha_ib_b[1,0] = alpha_ib_b[2]
+        Alpha_ib_b[1,2] = -alpha_ib_b[0]
+        Alpha_ib_b[2,0] = -alpha_ib_b[1]
+        Alpha_ib_b[2,1] = alpha_ib_b[0]
+
         # Obtain coordinate transformation matrix from the new attitude w.r.t. an
         # inertial frame to the old using Rodrigues' formula, (5.73)
         if mag_alpha > 1.0e-8:
@@ -392,15 +391,16 @@ def dead_reckoning_ecef_test(Fs, dr_temp, est_C_b_e, imu_meas, imu_error):
     # this produces a warning, we should fix it
     # mag_alpha = torch.sqrt(alpha_ib_b.detach().T @ alpha_ib_b.detach())
     mag_alpha = torch.norm(alpha_ib_b)
-    '''gradient'''
-    Alpha_ib_b = torch.tensor(
-        [
-            [0, -alpha_ib_b[2], alpha_ib_b[1]],
-            [alpha_ib_b[2], 0, -alpha_ib_b[0]],
-            [-alpha_ib_b[1], alpha_ib_b[0], 0],
-        ],
-        device=dev,
-    )
+    
+    Alpha_ib_b = torch.zeros(3,3)
+    
+    Alpha_ib_b[0,1] = -alpha_ib_b[2]
+    Alpha_ib_b[0,2] = alpha_ib_b[1]
+    Alpha_ib_b[1,0] = alpha_ib_b[2]
+    Alpha_ib_b[1,2] = -alpha_ib_b[0]
+    Alpha_ib_b[2,0] = -alpha_ib_b[1]
+    Alpha_ib_b[2,1] = alpha_ib_b[0]
+
     # Obtain coordinate transformation matrix from the new attitude w.r.t. an
     # inertial frame to the old using Rodrigues' formula, (5.73)
     if mag_alpha > 1.0e-8:

main.py
@@ -57,28 +57,29 @@ dataset_type = 'simudata_MEMS_3d_10hz.mat'
 indx_MB = True
 
 # 
-operation = 'TRAIN'
+# operation = 'TRAIN'
  
-# operation = 'TEST'
-# in_model = '0817_MEMS_withgrad_shuffle_lstm111_800_pvb_lkrelu_alltorchnorm_m2m_ifimuatt01001'
-# %matplotlib widget
+operation = 'TEST'
+in_model = '0823_MEMS_withgrad_shuffle_lstm111_1000_pvab_lkrelu_alltorchnorm_m2m_noifimuatt01001_loss123'
+Longer_test_traj_indx = True
+%matplotlib widget
 
 # operation = 'TRAIN_USEOLD'
-# in_model = '0814_real_withgrad_shuffle_lstm111_200__feedavpb_lkrelu_alltorchnorm_m2m_noifimuatt'
-# out_model = '0814_real_withgrad_shuffle_lstm111_200__feedavpb_lkrelu_alltorchnorm_m2m_noifimuatt_1'
+# in_model = '0817_bias_withgrad_shuffle_lstm111_200_pvab_lkrelu_alltorchnorm_m2m_ifimuatt01001_loss23_1'
+# out_model = '0817_bias_withgrad_shuffle_lstm111_200_pvab_lkrelu_alltorchnorm_m2m_ifimuatt01001_loss23_2'
 
 
 nograd_sd = False # True: with torch.no_grad, nograd    False: none, withgrad
 idx_shuffle = True
 indx_add_measerr = False
-indx_train_gnssgap = False
+indx_train_gnssgap = True
 
 idx_lossweight_coeff = ['1','1','1']
 
 idx_feedback_type = 'pvab'
 # idx_feedback_type = 'pvb'
 # idx_other_settings = '_feedavpb_lkrelu_alltorchnorm_m2m_noifimuatt0101'
-idx_other_settings = '_lkrelu_alltorchnorm_m2m_ifimuatt01001_onlyloss3'
+idx_other_settings = '_lkrelu_alltorchnorm_m2m_ifimuatt01001_loss23'
 
 
 idx_train_batch_size = 20
@@ -95,7 +96,7 @@ recurrent_kind = 'lstm'  # 'rnn' 'gru' 'lstm'
 
 
 idx_num_epochs = 200
-idx_learning_rate =1e-4
+idx_learning_rate =1e-5
 idx_weight_decay = 1e-8
 scheduler = "cosine_annealing 200"
 # scheduler = "step 100 0.1"
@@ -277,20 +278,21 @@ test_IMU = IMU[train_num:].to(dev)
 test_targets = T[train_num:].to(dev)
 # test_targets = T2[trainsets_num:]
 
-# Longer test traj
-# indx_test = idx_cut_test_traj
-# # test_time_traj = test_time_traj.reshape(indx_test,-1)
-# test_features = train_features.reshape(indx_test,-1,6)
-# test_IMU = train_IMU.reshape(indx_test,-1,Fs,6)
-# test_targets = train_targets.reshape(indx_test,-1,33)
+if Longer_test_traj_indx:
+    # Longer test traj
+    # indx_test = idx_cut_test_traj
+    # # test_time_traj = test_time_traj.reshape(indx_test,-1)
+    # test_features = train_features.reshape(indx_test,-1,6)
+    # test_IMU = train_IMU.reshape(indx_test,-1,Fs,6)
+    # test_targets = train_targets.reshape(indx_test,-1,33)
 
 
-# # # Longer test traj
-# indx_test = idx_cut_test_traj
-# # test_time_traj = test_time_traj.reshape(indx_test,-1)
-# test_features = test_features.reshape(indx_test,-1,6)
-# test_IMU = test_IMU.reshape(indx_test,-1,Fs,6)
-# test_targets = test_targets.reshape(indx_test,-1,33)
+    # # Longer test traj
+    indx_test = idx_cut_test_traj
+    # test_time_traj = test_time_traj.reshape(indx_test,-1)
+    test_features = test_features.reshape(indx_test,-1,6)
+    test_IMU = test_IMU.reshape(indx_test,-1,Fs,6)
+    test_targets = test_targets.reshape(indx_test,-1,33)
 
 print("data loaded")
 